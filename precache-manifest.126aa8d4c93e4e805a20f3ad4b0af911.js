db7ca75bdc104d177a01791897e3fdfdf4ef265a
hana Charaabi <charaabihana90@gmail.com>
2019-05-16 12:58:33 +0100

    Updates

---
+++ precache-manifest.126aa8d4c93e4e805a20f3ad4b0af911.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ad67839ec0212008438353a6e7018930",
+    "url": "/complex-credit-Card/index.html"
+  },
+  {
+    "revision": "a23105786f6a8877f373",
+    "url": "/complex-credit-Card/static/css/main.499ab09e.chunk.css"
+  },
+  {
+    "revision": "d2103c5e095edc1346e1",
+    "url": "/complex-credit-Card/static/js/2.a68ccf2f.chunk.js"
+  },
+  {
+    "revision": "a23105786f6a8877f373",
+    "url": "/complex-credit-Card/static/js/main.7f3a0e64.chunk.js"
+  },
+  {
+    "revision": "9e01019ac1fb95f3e8f5",
+    "url": "/complex-credit-Card/static/js/runtime~main.7e6ec308.js"
+  },
+  {
+    "revision": "f69d01d0330b09a4ed11b45e3f42ec29",
+    "url": "/complex-credit-Card/static/media/MasterCard.f69d01d0.png"
+  },
+  {
+    "revision": "6ac2533ae68698a1ffa4ae3f8ab17bf3",
+    "url": "/complex-credit-Card/static/media/creditCart.6ac2533a.png"
+  },
+  {
+    "revision": "97a3019713284167f8e679c1c53f3c8e",
+    "url": "/complex-credit-Card/static/media/puce.97a30197.png"
+  }
+]);
